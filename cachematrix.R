5cb6844df2bd8e025af23f3dd68eb4141ea1e407
sharno <sharnoby3@gmail.com>
2014-07-10 17:56:32 +0200

    made a closure matrix with cachable inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## helpful functions to create matrices with cachable inverses
+## so that if inverse was calculated before it's stored as cache
 
-## Write a short comment describing this function
-
+## makes a matrix closure to be stored with its cached inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## gets inverse if it's cached or calculates inverse and cache it if it's not
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if (!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
